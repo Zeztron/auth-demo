3f5ea8a7fc764defe6bcbdc164b67cd15a8b6593
Harsh Patel <pateltharsh@gmail.com>
2018-10-22 22:50:45 +0000

    Created post route for the register form

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true })
 
 var app = express();
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(require("express-session")({
@@ -23,6 +23,9 @@ app.use(require("express-session")({
 passport.serializeUser(User.serializeUser);
 passport.deserializeUser(User.deserializeUser);
 
+// ===================
+//      ROUTES
+// ===================
 
 app.get("/", function(req, res) {
     res.render("home");
@@ -30,6 +33,24 @@ app.get("/", function(req, res) {
 
 app.get("/secret", function(req, res) {
     res.render("secret");
+});
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render('register');
+        } 
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/secret");
+        });
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
